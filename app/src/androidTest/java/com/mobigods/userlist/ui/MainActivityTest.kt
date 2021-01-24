7250a3795999f8b651b9338ada98bd94c69017bc
Emmaunuel Ozibo <emmanuel022forreal@gmail.com>
2021-01-24 11:12:42 +0100

    Applied spotless

--- app/src/androidTest/java/com/mobigods/userlist/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/mobigods/userlist/ui/MainActivityTest.kt
@@ -2,13 +2,10 @@ package com.mobigods.userlist.ui
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @HiltAndroidTest
@@ -18,10 +15,9 @@ class MainActivityTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-
     @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity>
-            = ActivityScenarioRule(MainActivity::class.java)
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
 
     @Before
     fun setUp() {
